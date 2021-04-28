c452f0ebd9dcbf0a549ae8e6c4400fd25a68222f
Jeremias Erbs <jerry@badabam.de>
2021-04-28 14:39:28 +0200

    Implement toUpperCase in output

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const form = document.querySelector('[data-form]')
 const box = document.querySelector('[data-box]')
 const input = document.querySelector('[name="class"]')
+const output = document.querySelector('[data-output]')
 
 form.addEventListener('submit', event => {
   event.preventDefault()
   box.className = 'box ' + input.value
+  const inputText = input.value // 'hello'
   input.value = ''
   input.focus()
+  const outputText = inputText.toUpperCase()
+  output.value = outputText
 })
