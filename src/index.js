2c5114d017e43838151aed95b6152a9c8d7b993f
Jeremias Erbs <jerry@badabam.de>
2021-04-28 13:02:25 +0200

    Implement box with classes from input

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const form = document.querySelector('[data-form]')
-const header = document.querySelector('[data-header]')
-const input = document.querySelector('[name="header-text"]')
+const box = document.querySelector('[data-box]')
+const input = document.querySelector('[name="class"]')
 
 form.addEventListener('submit', event => {
   event.preventDefault()
-  header.innerText = input.value
+  box.className = 'box ' + input.value
+  input.value = ''
+  input.focus()
 })
