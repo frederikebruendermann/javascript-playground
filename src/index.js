f77f2ae37ee762dfb73406e136b5c16948ef6e79
Jeremias Erbs <jerry@badabam.de>
2021-04-28 12:04:15 +0200

    Implement header text logic

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-console.clear()
-const box = document.querySelector('.box')
-const buttonRotation = document.querySelector('.button-rotation')
-const buttonBackground = document.querySelector('.button-background')
-const buttonRadius = document.querySelector('.button-radius')
+const form = document.querySelector('[data-form]')
+const header = document.querySelector('[data-header]')
+const input = document.querySelector('[name="header-text"]')
 
-buttonRotation.addEventListener('click', () => {
-  box.classList.toggle('rotate')
-})
-
-buttonBackground.addEventListener('click', () => {
-  box.classList.toggle('blue')
-})
-
-buttonRadius.addEventListener('click', () => {
-  box.classList.toggle('radius')
+form.addEventListener('submit', event => {
+  event.preventDefault()
+  header.innerText = input.value
 })
